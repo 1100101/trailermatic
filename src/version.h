b1e6710d350f7fee99103b337f822560a27d1be6
Frank Aurich <1100101@gmail.com>
2015-03-18 12:04:11 +0100

    Use git revisions in version.h

--- src/version.h
+++ src/version.h
@@ -1,2 +1,3 @@
-#define SVN_REVISION          ""
-#define LONG_VERSION_STRING   "0.2x ()"
+#define SVN_REVISION          "4fa0015"
+#define SHORT_VERSION_STRING  "0.2.1"
+#define LONG_VERSION_STRING   "0.2.1 (4fa0015)"
